e62adfd97c02b642aca0ebe4e9f9c9c855fe0470
Victor Hugo <vct.bta@gmail.com>
2018-06-18 15:28:28 -0300

    Adiciona e estiliza o texto '(clique abaixo para copiar)' do shortcode gerado

--- acf-crosswords/fields/partials/acf-crossword-geneated-shortcode.php
+++ acf-crosswords/fields/partials/acf-crossword-geneated-shortcode.php
@@ -1,6 +1,9 @@
 <div class="crossword-editor-generated-shortcode">
 	<div class="crossword-editor-generated-shortcode-inner">
-		<div>Shortcode gerado:</div>
+		<div>
+			Shortcode gerado:
+			<div class="highlighted">(clique abaixo para copiar)</div>
+		</div>
 		<input class="crossword-editor-generated-shortcode-box clipboarder" readonly
 			value="<?php echo esc_attr("[palavra-cruzada id=\"{$_GET['post']}\"]") ?>" />
 	</div>
