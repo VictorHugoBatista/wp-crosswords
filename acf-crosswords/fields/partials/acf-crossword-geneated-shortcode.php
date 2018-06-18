9e743807d41309fd832906e06728b42a47ee3b00
Victor Hugo <vct.bta@gmail.com>
2018-06-18 14:55:39 -0300

    Estiliza sessão de shortcode gerado

--- acf-crosswords/fields/partials/acf-crossword-geneated-shortcode.php
+++ acf-crosswords/fields/partials/acf-crossword-geneated-shortcode.php
@@ -1,6 +1,8 @@
-<div>
-	Shortcode gerado:
-	<div>
-		[palavra-cruzada id="<?php echo $_GET['post'] ?>"]
+<div class="crossword-editor-generated-shortcode">
+	<div class="crossword-editor-generated-shortcode-inner">
+		<div>Shortcode gerado:</div>
+		<div class="crossword-editor-generated-shortcode-box">
+			[palavra-cruzada id="<?php echo $_GET['post'] ?>"]
+		</div>
 	</div>
 </div>
