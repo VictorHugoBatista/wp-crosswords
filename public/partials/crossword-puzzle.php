e61aeac4ac1270b5ac0260c7e6b1ca33553435fa
Victor Hugo <vct.bta@gmail.com>
2018-06-18 18:11:57 -0300

    Adiciona form e formata os names dos campos da palavra cruzada do shortcode

--- public/partials/crossword-puzzle.php
+++ public/partials/crossword-puzzle.php
@@ -1,15 +1,19 @@
-<table class="crossword-puzzle">
-	<?php foreach ($crossword as $row) : ?>
-	<tr>
-		<?php foreach ($row as $cell) : ?>
-		<?php if ('' !== $cell) : ?>
-		<td class="bordered">
-			<input type="text" class="crossword-puzzle-cell" value="<?php echo $cell ?>" />
-		</td>
-		<?php else : ?>
-		<td></td>
-		<?php endif ?>
+<form action="">
+	<table class="crossword-puzzle">
+		<?php foreach ($crossword as $key_row => $row) : ?>
+		<tr>
+			<?php foreach ($row as $key_cell => $cell) : ?>
+			<?php if ('' !== $cell) : ?>
+			<td class="bordered">
+				<input type="text" class="crossword-puzzle-cell" required
+					name="crossword-puzzle-cell[<?php echo $key_row ?>][<?php echo $key_cell ?>]" />
+			</td>
+			<?php else : ?>
+			<td></td>
+			<?php endif ?>
+			<?php endforeach ?>
+		</tr>
 		<?php endforeach ?>
-	</tr>
-	<?php endforeach ?>
-</table>
+	</table>
+	<button>Resolver</button>
+</form>
