8c01c2924884e9ea725dde27a67d9d0583cbdbd1
Victor Hugo <vct.bta@gmail.com>
2018-06-18 18:01:19 -0300

    Exibe a estrutura de caça palavras, com bordas apenas nas letras preenchidas na administração

--- public/partials/crossword-puzzle.php
+++ public/partials/crossword-puzzle.php
@@ -1,8 +1,14 @@
-<table>
+<table class="crossword-puzzle">
 	<?php foreach ($crossword as $row) : ?>
 	<tr>
 		<?php foreach ($row as $cell) : ?>
-		<td><?php echo $cell ?></td>
+		<?php if ('' !== $cell) : ?>
+		<td class="bordered">
+			<?php echo $cell ?>
+		</td>
+		<?php else : ?>
+		<td></td>
+		<?php endif ?>
 		<?php endforeach ?>
 	</tr>
 	<?php endforeach ?>
