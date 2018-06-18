8b7b532607ea3b2fed932795d09e86d565f23e55
Victor Hugo <vct.bta@gmail.com>
2018-06-18 15:22:41 -0300

    Ajusta estado hover do shortcode gerado

--- acf-crosswords/fields/partials/acf-crossword-geneated-shortcode.php
+++ acf-crosswords/fields/partials/acf-crossword-geneated-shortcode.php
@@ -1,7 +1,7 @@
 <div class="crossword-editor-generated-shortcode">
 	<div class="crossword-editor-generated-shortcode-inner">
 		<div>Shortcode gerado:</div>
-		<input class="crossword-editor-generated-shortcode-box clipboarder"
+		<input class="crossword-editor-generated-shortcode-box clipboarder" readonly
 			value="<?php echo esc_attr("[palavra-cruzada id=\"{$_GET['post']}\"]") ?>" />
 	</div>
 </div>
