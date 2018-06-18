20c44962641541a895cc3f644be377635832d973
Victor Hugo <vct.bta@gmail.com>
2018-06-18 17:53:19 -0300

    Exibe tabela com os dados da palavra cruzada cadastrada

--- public/partials/crossword-puzzle.php
+++ public/partials/crossword-puzzle.php
@@ -1,1 +1,9 @@
-|--- id=<?php echo $data['id'] ?> ---|
+<table>
+	<?php foreach ($crossword as $row) : ?>
+	<tr>
+		<?php foreach ($row as $cell) : ?>
+		<td><?php echo $cell ?></td>
+		<?php endforeach ?>
+	</tr>
+	<?php endforeach ?>
+</table>
